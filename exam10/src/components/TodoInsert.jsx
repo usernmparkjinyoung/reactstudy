451dcbea495f2c13483c2077a2953ae69f21b869
jyp <80496834+usernmparkjinyoung@users.noreply.github.com>
2022-03-21 00:49:14 +0900

    dddd

--- exam10/src/components/TodoInsert.jsx
+++ exam10/src/components/TodoInsert.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md'; // 리액트 아이콘, https://react-icons.github.io/react-icons/
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+    const [ value, setValue ] = useState('');
+
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(
+        e => { 
+            onInsert(value);
+            setValue('');
+
+            // submit는 브라우저에서 새로고침을 발생시킴 
+            // 이를 방지하기 위해 이 함수를 호출   
+            e.preventDefault();
+        },
+        [onInsert, value],
+    )
+
+    /* 
+    const onClick = useCallback(
+        () => {
+            onInsert(value);
+            setValue('');
+        }, 
+        [onInsert, value]
+    )
+    */ 
+
     return (
-        <form className="TodoInsert">
-            <input placeholder="할 일을 입력하세요" />
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input 
+            placeholder="할 일을 입력하세요"
+            value={value}
+            onChange={onChange}
+            />
             <button type="submit">
                 <MdAdd />
             </button>
