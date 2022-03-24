6e4c5849b4818f333d1bbc17ec96ed09d9beb064
jyp <80496834+usernmparkjinyoung@users.noreply.github.com>
2022-03-24 23:24:04 +0900

    dd

--- exam13/src/App.js
+++ exam13/src/App.js
@@ -4,6 +4,11 @@ import About from "./About";
 import Layout from './Layout';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import Articles from './pages/Articles';
+import Article from './pages/Article';
+import Login from './pages/Login';
+import MyPage from './pages/MyPage';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (  
@@ -14,9 +19,17 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/profiles/:username" element={<Profile />} />
         </Route>
-      </Routes>   
+        <Route path="/articles" element={<Articles />}>
+          <Route path=":id" element={<Article />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>    
     </div>
   );
 }
+
+// 1. 기본적으로 extact기능이 있고 extact를 쓰지 않으려면 *를 쓴다 
 
 export default App;
